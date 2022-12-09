08e1f4f7420d4cf87d5115c9618a16f2a88c82ba
as00r <as00r.dev@gmail.com>
2022-12-10 01:49:53 +0530

    Refactor

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 window.addEventListener("load", () => {
 	const canvas = document.getElementById("canvas");
 	const context = canvas.getContext("2d");
-	canvas.width = window.innerWidth * 0.8;
-	canvas.height = window.innerHeight * 0.8;
-	//canvas center
-	const cx = canvas.width / 2;
-	const cy = canvas.height / 2;
 
 	//canvas settings
+	canvas.width = window.innerWidth * 0.8;
+	canvas.height = window.innerHeight * 0.8;
 	context.strokeStyle = "green";
 	context.lineWidth = 10;
 	context.lineCap = "round";
 
+	//canvas center
+	const cx = canvas.width / 2;
+	const cy = canvas.height / 2;
+
 	//effect settings
-	const size = 200;
-	const sides = 5;
-	const maxDepth = 50;
+	const effect = {
+		size: 200,
+		sides: 5,
+		maxDepth: 50,
+	};
 
 	// notes
 	// -----
@@ -29,15 +32,9 @@ window.addEventListener("load", () => {
 	// context.fillRect(0, 0, canvas.width, canvas.height);
 
 	function drawBranch(depth) {
-		if (depth > maxDepth) return;
-		context.beginPath();
-		context.moveTo(0, 0);
-		context.lineTo(size, 0);
-		context.stroke();
-		context.translate(100, 0);
-		context.rotate(0.5);
-		context.scale(0.9, 0.9);
-
+		if (depth > effect.maxDepth) return;
+		doTransformations(100, 0, 0.5, 0.9, 0.9);
+		drawLine(0, 0);
 		drawBranch(depth + 1);
 	}
 	drawBranch(0);
@@ -45,6 +42,19 @@ window.addEventListener("load", () => {
 	// for (let i = 0; i < sides; i++) {
 	// 	context.rotate((Math.PI * 2) / sides);
 	// }
+
+	function drawLine(x, y) {
+		context.beginPath();
+		context.moveTo(x, y);
+		context.lineTo(effect.size, 0);
+		context.stroke();
+	}
+
+	function doTransformations(tx, ty, angle, sx, sy) {
+		context.translate(100, 0);
+		context.rotate(0.5);
+		context.scale(0.9, 0.9);
+	}
 
 	context.restore();
 });
